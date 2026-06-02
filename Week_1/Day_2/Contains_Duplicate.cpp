1227c532349bae69e6c7db8cf5026eb2a666c039
ShivamKumarMahto7 <sk9529503122@gmail.com>
2026-06-02 17:06:13 +0530

    Create Contains_Duplicate.cpp

---
+++ Week_1/Day_2/Contains_Duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int count =0;
+        sort(nums.begin(),nums.end());
+        for(int i = 0 ; i<n-1;i++){
+            if(nums[i]== nums[i+1]){
+               count++;
+            }
+           
+        }
+        if(count==0){
+            return false ;
+        }
+        return true;
+        
+    }
+};

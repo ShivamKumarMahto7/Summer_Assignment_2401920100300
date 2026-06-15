7bab9e4852e6a8e7890a24c0cd3865c47cc37313
ShivamKumarMahto7 <sk9529503122@gmail.com>
2026-06-15 23:54:34 +0530

    Create Reverse_Linked_List.cpp

---
+++ Week_3/Day_1/Reverse_Linked_List.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL){
+            return head ;
+        }
+        
+        ListNode* prev = NULL;
+        ListNode* curr = head ;
+        while(curr!=NULL){
+            ListNode* fur = curr->next;
+            curr->next = prev;
+            prev = curr ;
+            curr=fur ;
+
+        }
+        
+        return prev;
+    }
+};
